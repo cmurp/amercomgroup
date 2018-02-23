67da691578672cc92681b423c29618160f16a9a6
ctmurphy77 <rajonwitness@gmail.com>
2018-02-23 16:54:06 -0500

    All routing and links done

--- routes/markets.js
+++ routes/markets.js
@@ -1,3 +1,31 @@
 module.exports = function (req, res) {
-  // do stuff
+    switch(req.params.markets){
+        case 'restaurants':
+            res.render('markets/restaurants', { title: 'Restaurants' });
+            break;
+        case 'hotels':
+            res.render('markets/hotels', { title: 'Hotels' });
+            break;
+        case 'gyms':
+            res.render('markets/gyms', { title: 'Gyms' });
+            break;
+        case 'offices':
+            res.render('markets/offices', { title: 'Offices' });
+            break;
+        case 'medical':
+            res.render('markets/medical', { title: 'Medical' });
+            break;
+        case 'apartments':
+            res.render('markets/apartments', { title: 'Apartments' });
+            break;
+        case 'salons':
+            res.render('markets/salons', { title: 'Salons' });
+            break;
+        case 'mobile-home-communities':
+            res.render('markets/mobile_homes', { title: 'Mobile Home Communities' });
+            break;
+        default:
+            res.render('markets/markets', { title: 'Markets' });
+            break;
+    }
 };
--- routes/services.js
+++ routes/services.js
@@ -1,4 +1,16 @@
 module.exports = function (req, res) {
-    console.log("TEST");
-    res.render('services/' + req.params.service);
+    switch(req.params.services){
+        case 'american_communications':
+            res.render('services/ac', { title: 'American Communications' });
+            break;
+        case 'american_satellite_concepts':
+            res.render('services/asc', { title: 'American Satellite Concepts' });
+            break;
+        case 'american_wireless_synergy':
+            res.render('services/aws', { title: 'American Wireless Synergy' });
+            break;
+        default:
+            res.render('services/services', { title: 'Services' });
+            break;
+    }
 };
